7ce6ed85dea5ebd526d4ac159f4994fe998af0e8
Sravanthikoduru08 <161932236+Sravanthikoduru08@users.noreply.github.com>
2026-07-24 21:50:01 +0530

    Add files via upload

---
+++ crypto 7.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Use frequency analysis.\n");
+    printf("1. Most frequent symbol -> E\n");
+    printf("2. Find THE\n");
+    printf("3. Guess common words.\n");
+    printf("4. Continue replacing symbols until plaintext is obtained.\n");
+
+    return 0;
+}
